56809ae892caf85dc196427c1e111d6d2cbe1683
TheRadam <theradam@protonmail.com>
2025-11-28 20:05:55 +0000

    Page Generation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use std::process::{Command, ExitStatus};
+use std::fs;
+use std::process::{Command};
 use std::time::SystemTime;
 use libsql::{Builder};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<libsql::Error>> {
     let start = SystemTime::now();
     let url = std::env::var("TURSO_DATABASE_URL").expect("TURSO_DATABASE_URL must be set");
-    let token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set");
 
     let mut rows;
     {
-        let db = Builder::new_remote(url, token)
-            .build()
-            .await?;
+        let db = match std::env::var("DEV_MODE").unwrap().as_str() {
+            "0" => {
+                let token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set");
+                Builder::new_remote(url, token).build().await?
+            }
+            _ => Builder::new_local(url).build().await?
+        };
+
         let conn = db.connect()?;
 
         let lichenised_id = select_random_id();
@@ -32,8 +40,10 @@ async fn main() -> Result<(), Box<libsql::Error>> {
         println!("Row {}: {:?}", i, row.get_value(i).unwrap());
     }
 
-    get_image(*row.get_value(0).unwrap().as_integer().unwrap() as u32);
+    let name = row.get_value(1).unwrap();
+    let image = get_image(*row.get_value(0).unwrap().as_integer().unwrap() as u32);
 
+    generate_page(image, name.as_text().unwrap().clone());
 
     println!("Time Taken: {}ms", start.elapsed().unwrap().as_millis());
     Ok(())
@@ -43,7 +53,7 @@ fn select_random_id() -> u32 {
     rand::random_range(1..=2372981) as u32
 }
 
-fn get_image(id: u32) -> ExitStatus {
+fn get_image(id: u32) -> String {
     let jpeg = Regex::new(r".jpeg").unwrap();
     let png = Regex::new(r".png").unwrap();
     let list = Command::new("aws")
@@ -70,7 +80,24 @@ fn get_image(id: u32) -> ExitStatus {
         .arg("cp")
         .arg("--no-sign-request")
         .arg(format!("s3://inaturalist-open-data/photos/{}/original.{}", id, extension))
-        .arg(".")
+        .arg(format!("image.{}", extension))
         .status()
-        .expect("Failed to download image")
+        .expect("Failed to download image");
+
+    format!("image.{}", extension)
+}
+
+fn generate_page(file_name: String, taxon_name: String) {
+    let path = Path::new("template.html");
+    let path_new_file = Path::new("new_index.html");
+    let mut file = File::create(path_new_file).unwrap();
+    let contents = fs::read_to_string(path)
+        .expect("Should have been able to read the file");
+
+    let image_src = Regex::new(r"#IMAGE#").unwrap();
+    let name = Regex::new(r"#NAME#").unwrap();
+    let x = image_src.replace(contents.as_str(), file_name);
+    let y = name.replace(x.as_ref(), taxon_name);
+
+    file.write_all(y.as_bytes()).expect("Should have been able to write file");
 }
