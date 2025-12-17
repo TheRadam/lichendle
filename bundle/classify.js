7a923fadc5879b29c18f695f93a289529bd3417f
TheRadam <theradam@protonmail.com>
2025-12-17 20:49:52 +0000

    Split input checking

--- bundle/classify.js
+++ bundle/classify.js
@@ -1,13 +1,25 @@
 function classify(name) {
-    let genus = document.getElementById("genus").value;
-    let species = document.getElementById("species").value;
-    console.log(genus);
-    console.log(species);
-    console.log([genus, species].join(" ").toLowerCase() == name.toLowerCase());
+    name = name.trim().toLowerCase();
+    let genus = document.getElementById("genus").value.trim().toLowerCase();
+    let species = document.getElementById("species").value.trim().toLowerCase();
+    let [expectedGenus, expectedSpecies] = name.split(" ");
 
-    if ([genus, species].join(" ").toLowerCase() == name.toLowerCase()) {
+    setBorder(expectedGenus == genus, document.getElementById("genus"));
+    setBorder(expectedSpecies == species, document.getElementById("species"));
+
+    if (expectedGenus == genus && expectedSpecies == species) {
         document.getElementById("result").innerHTML = "<br><span class='text-emerald-600'>YOU GOT THAT LICHEN!</span>"
     } else {
         document.getElementById("result").innerHTML = "<br><span class='text-red-600'>WRONG!</span>"
+    }
+}
+
+function setBorder(condition, input) {
+    if (condition) {
+        input.classList.remove('border-red-600');
+        input.classList.add('border-emerald-600');
+        input.setAttribute("disabled", true)
+    } else {
+        input.classList.add('border-red-600');
     }
 }
