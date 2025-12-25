df075735acd2d8527d02a9525e5e8f3433634679
TheRadam <theradam@protonmail.com>
2025-12-25 15:38:43 +0000

    debugging paths

--- ui-tests/tests/index.spec.ts
+++ ui-tests/tests/index.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page, baseURL }) => {
+  const fs = require('fs');
+
+  fs.readdirSync("file://html").forEach(file => {
+    // will also include directory names
+    console.log(file);
+  });
+
   await page.goto(baseURL!);
 });
 
