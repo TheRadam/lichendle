620b69ce2efed579d2377ac053b2bfe19688db56
TheRadam <theradam@protonmail.com>
2025-12-25 13:34:42 +0000

    debugging paths

--- ui-tests/tests/index.spec.ts
+++ ui-tests/tests/index.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page, baseURL }) => {
   console.info('process env', process.env.PLAYWRIGHT_TEST_BASE_URL);
-  await page.goto(baseURL);
+  await page.goto(process.env.PLAYWRIGHT_TEST_BASE_URL);
 });
 
 test('has title', async ({ page }) => {
