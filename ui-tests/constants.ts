f0ff3820e1ec05c99890424cc5a367d1327c4be7
TheRadam <theradam@protonmail.com>
2025-12-26 19:20:11 +0000

    Added some tests for basic flows

---
+++ ui-tests/constants.ts
@@ -0,0 +1,10 @@
+
+
+export const CONSTANTS = {
+    CORRECT_GENUS: 'Lobaria',
+    CORRECT_SPECIES: 'pulmonaria',
+    INCORRECT_GENUS: 'Evernia',
+    INCORRECT_SPECIES: 'prunastri',
+    SUCCESS_TEXT: 'YOU GOT THAT LICHEN!',
+    FAILURE_TEXT: 'WRONG!',
+};
--- ui-tests/tests/index.spec.ts
+++ ui-tests/tests/index.spec.ts
@@ -1,4 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { assertCorrectGenus, assertIncorrectGenus, fillCorrectGenus } from "../helpers/genusHelper";
+import { assertCorrectSpecies, assertIncorrectSpecies, fillSpeciesInput } from "../helpers/speciesHelper";
+import { assertIncorrectResult, assertCorrectResult } from "../helpers/assertions"
+import { CONSTANTS } from '../constants';
+
+
 
 test.beforeEach(async ({ page, baseURL }) => {
   await page.goto(baseURL!);
@@ -6,4 +12,45 @@ test.beforeEach(async ({ page, baseURL }) => {
 
 test('has title', async ({ page }) => {
   await expect(page).toHaveTitle("Lichendle");
+});
+
+test('empty input should fail', async ({ page }) => {
+  await page.getByText("Classify").click();
+
+  await assertIncorrectGenus(page);
+  await assertIncorrectSpecies(page);
+  await assertIncorrectResult(page);
+});
+
+test('correct input should succeed', async ({ page }) => {
+  await fillCorrectGenus(page, CONSTANTS.CORRECT_GENUS);
+  await fillSpeciesInput(page, CONSTANTS.CORRECT_SPECIES);
+
+  await page.getByText("Classify").click();
+
+  await assertCorrectResult(page);
+  await assertCorrectGenus(page);
+  await assertCorrectSpecies(page);
+});
+
+test('correct genus input should fail', async ({ page }) => {
+  await fillCorrectGenus(page, CONSTANTS.CORRECT_GENUS);
+  await fillSpeciesInput(page, CONSTANTS.INCORRECT_SPECIES);
+
+  await page.getByText("Classify").click();
+
+  await assertIncorrectResult(page);
+  await assertCorrectGenus(page);
+  await assertIncorrectSpecies(page);
+});
+
+test('correct species input should fail', async ({ page }) => {
+  await fillCorrectGenus(page, CONSTANTS.INCORRECT_GENUS);
+  await fillSpeciesInput(page, CONSTANTS.CORRECT_SPECIES);
+
+  await page.getByText("Classify").click();
+
+  await assertIncorrectResult(page);
+  await assertCorrectSpecies(page);
+  await assertIncorrectGenus(page);
 });
